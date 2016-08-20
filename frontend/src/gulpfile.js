b0ca3962fab0a3861b32aec8c91c34fffa1a7651
Mentor <mentor@palokaj.co>
2016-08-20 23:25:09 +0200

    Enforce my subjective beauty ideals on the gulpfile

--- frontend/src/gulpfile.js
+++ frontend/src/gulpfile.js
@@ -22,52 +22,49 @@ const paths = {
 	}
 }
 
-gulp.task('default', function() {
+gulp.task( 'default', () => {
   // Gulp default
   console.log( 'Gulp works, but this command does nothing' )
-})
+} )
 
 
-gulp.task('clean', function(cb) {
+gulp.task( 'clean', ( cb ) => {
 	rimraf( paths.build.root, cb )
-})
+}) 
 
-gulp.task('styles', ['clean'], function(cb) {
+gulp.task( 'styles', ['clean'], ( cb ) => {
 	console.log( 'writing styles from ' + paths.source.scss )
 	console.log( 'writing styles to ' + paths.build.css )
 	return gulp.src( paths.source.scss )
 	.pipe( sass().on( 'error', sass.logError ) )
 	.pipe( gulp.dest( paths.build.css ) )
-})
+} )
 
-gulp.task('scripts', ['clean'], function(cb) {
+gulp.task( 'scripts', ['clean'], ( cb ) => {
 	console.log( 'writing scripts from ' + paths.source.js )
 	console.log( 'writing scripts to ' + paths.build.js )
 	return gulp.src( paths.source.js )
-	.pipe(sourcemaps.init())
+	.pipe( sourcemaps.init() )
 	.pipe( babel({
 		presets: ['es2015']
 	}))
-	.pipe(concat('all.js'))
-	.pipe(sourcemaps.write('.'))
+	.pipe( concat('all.js') )
+	.pipe( sourcemaps.write('.') )
 	.pipe( gulp.dest( paths.build.js ) )
-})
+} )
 
-
-gulp.task('views', ['clean'], function (cb) {
+gulp.task( 'views', ['clean'], ( cb ) => {
 	console.log( 'writing views from ' + paths.source.pug )
 	console.log( 'writing views to ' + paths.build.html )
   return gulp.src( paths.source.pug )
-  .pipe(pug({}))
+  .pipe( pug({}) )
   .pipe( gulp.dest( paths.build.html ) )
-});
+} )
 
-gulp.task('build', ['clean', 'styles', 'scripts', 'views'], (  ) => {
-  console.log( "Build complete" )
-});
+gulp.task('build', ['clean', 'styles', 'scripts', 'views'], () => {})
 
-gulp.task('watch', ['build'], function() {
-  return watch(paths.source.root, function() {
-    gulp.start('build');
-  });
-});
+gulp.task( 'watch', ['build'], () => {
+  return watch( paths.source.root, () => {
+    gulp.start( 'build' )
+  } )
+} )
