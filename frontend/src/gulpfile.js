827eed862373b192ddc5aa788d706d66ae80e9bb
Mentor <mentor@palokaj.co>
2016-08-20 23:17:48 +0200

    Views gulp done

--- frontend/src/gulpfile.js
+++ frontend/src/gulpfile.js
@@ -4,12 +4,13 @@ const sass 		 = require( 'gulp-sass' )
 const babel 	 = require( 'gulp-babel' )
 const sourcemaps = require( 'gulp-sourcemaps' )
 const concat 	 = require( 'gulp-concat' )
+const pug 		 = require( 'gulp-pug' )
 
 const paths = {
 	source: {
-		scss: 	__dirname + '/styles/*,scss',
-		js: 	__dirname + '/scripts/*',
-		pug: 	__dirname + '/views/'
+		scss: 	__dirname + '/styles/*.scss',
+		js: 	__dirname + '/scripts/*.js',
+		pug: 	__dirname + '/views/*.pug'
 	},
 	build: {
 		root: 	__dirname + '/../public/',
@@ -29,14 +30,16 @@ gulp.task('clean', function(cb) {
 	rimraf( paths.build.root, cb )
 })
 
-gulp.task('styles', function(cb) {
+gulp.task('styles', ['clean'], function(cb) {
+	console.log( 'writing styles from ' + paths.source.scss )
 	console.log( 'writing styles to ' + paths.build.css )
 	return gulp.src( paths.source.scss )
 	.pipe( sass().on( 'error', sass.logError ) )
 	.pipe( gulp.dest( paths.build.css ) )
 })
 
-gulp.task('scripts', function(cb) {
+gulp.task('scripts', ['clean'], function(cb) {
+	console.log( 'writing scripts from ' + paths.source.js )
 	console.log( 'writing scripts to ' + paths.build.js )
 	return gulp.src( paths.source.js )
 	.pipe(sourcemaps.init())
@@ -47,6 +50,27 @@ gulp.task('scripts', function(cb) {
 	.pipe(sourcemaps.write('.'))
 	.pipe( gulp.dest( paths.build.js ) )
 })
+
+
+gulp.task('views', ['clean'], function (cb) {
+	console.log( 'writing views from ' + paths.source.pug )
+	console.log( 'writing views to ' + paths.build.html )
+  return gulp.src( paths.source.pug )
+  .pipe(pug({}))
+  .pipe( gulp.dest( paths.build.html ) )
+});
+
+gulp.task('build', ['clean', 'styles', 'scripts', 'views'], (  ) => {
+  console.log( "Build complete" )
+});
+
+// gulp.task('views', function buildHTML() {
+//   return gulp.src( paths.source.views )
+//   .pipe(pug({
+//     // Your options in here. 
+//   }))
+// });
+
 
 // gulp.task('default', () => {
 //     return gulp.src('src/**/*.js')
@@ -88,17 +112,17 @@ gulp.task('scripts', function(cb) {
 
 // gulp.task('default', ['build']);
 
-gulp.task('build', ['clean'], function() {
+// gulp.task('build', ['clean'], function() {
 
-	// Compile app
-	return gulp
-	.src(paths.babel)
-	.pipe(babel({
-		presets: ['es2016', 'react']
-	}))
-	.pipe(gulp.dest(paths.build))
+// 	// Compile app
+// 	return gulp
+// 	.src(paths.babel)
+// 	.pipe(babel({
+// 		presets: ['es2016', 'react']
+// 	}))
+// 	.pipe(gulp.dest(paths.build))
 
-})
+// })
 
 // 	// Compile sass
 // 	return gulp.src( __dirname + '/src/frontend/*.scss' )
