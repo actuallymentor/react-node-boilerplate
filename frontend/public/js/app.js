537465f5dfd6f2d359110c67b0a2df0896940f68
Mentor <mentor@palokaj.co>
2016-08-22 19:03:10 +0200

    Basic header

---
+++ frontend/public/js/app.js
@@ -0,0 +1,2 @@
+"use strict";var app=function(){console.log("This app has no functionality")};app(),console.log("I am not very useful");
+//# sourceMappingURL=app.js.map
--- frontend/src/gulpfile.js
+++ frontend/src/gulpfile.js
@@ -7,26 +7,33 @@ const sourcemaps = require( 'gulp-sourcemaps' )
 const concat 	 = require( 'gulp-concat' )
 const pug 		 = require( 'gulp-pug' )
 const watch 	 = require( 'gulp-watch' )
+const uglify 	 = require( 'gulp-uglify' )
+const uglifycss  = require( 'gulp-uglifycss' )
 
 // Put all relevamt paths in one place
 const paths = {
 	source: {
-		root: __dirname,
-		scss: 	__dirname + '/styles/*.scss',
-		js: 	__dirname + '/scripts/*.js',
-		pug: 	__dirname + '/views/*.pug'
+		root: 	__dirname,
+		styles: __dirname + '/styles/*',
+		js: {
+			app: 	__dirname + '/scripts/*.js',
+			deps: 	__dirname + '/scripts/dependencies/*.js'
+		},
+		pug: 	__dirname + '/views/*.pug',
+		fonts: __dirname + '/fonts/**/*'
 	},
 	build: {
 		root: 	__dirname + '/../public/',
 		css: 	__dirname + '/../public/css/',
 		js: 	__dirname + '/../public/js/',
-		html: 	__dirname + '/../public/'
+		html: 	__dirname + '/../public/',
+		fonts:  __dirname + '/../public/fonts/'
 	}
 }
 
 // Make the default task listen for changes
 gulp.task( 'default', () => {
-  gulp.start( 'watch' )
+	gulp.start( 'watch' )
 } )
 
 // Rmrf task that deletes the build files before tasks
@@ -36,36 +43,61 @@ gulp.task( 'clean', ( cb ) => {
 
 // Compiling and writing styles
 gulp.task( 'styles', ['clean'], ( cb ) => {
-	return gulp.src( paths.source.scss )
+	return gulp.src( paths.source.styles )
+	.pipe( sourcemaps.init() )
 	.pipe( sass().on( 'error', sass.logError ) )
+	.pipe( concat('styles.css') )
+	.pipe( uglifycss({}) )
+	.pipe( sourcemaps.write('.') )
 	.pipe( gulp.dest( paths.build.css ) )
+} )
+
+// Compiling and writing styles
+gulp.task( 'fonts', ['clean'], ( cb ) => {
+	return gulp.src([paths.source.fonts])
+	.pipe(gulp.dest(paths.build.fonts));
+} )
+
+// Compiling and writing scripts, they are all combined into one all.js file
+gulp.task( 'dependency-scripts', ['clean'], ( cb ) => {
+	return gulp.src( paths.source.js.deps )
+	.pipe( sourcemaps.init() )
+	.pipe( babel({
+		presets: ['es2015'],
+		compact: true
+	}))
+	.pipe( concat('dependencies.js') )
+	.pipe( uglify() )
+	.pipe( sourcemaps.write('.') )
+	.pipe( gulp.dest( paths.build.js ) )
 } )
 
 // Compiling and writing scripts, they are all combined into one all.js file
 gulp.task( 'scripts', ['clean'], ( cb ) => {
-	return gulp.src( paths.source.js )
+	return gulp.src( paths.source.js.app )
 	.pipe( sourcemaps.init() )
 	.pipe( babel({
 		presets: ['es2015']
 	}))
-	.pipe( concat('all.js') )
+	.pipe( concat('app.js') )
+	.pipe( uglify() )
 	.pipe( sourcemaps.write('.') )
 	.pipe( gulp.dest( paths.build.js ) )
 } )
 
 // Compiling and writing the pug synax views to html
 gulp.task( 'views', ['clean'], ( cb ) => {
-  return gulp.src( paths.source.pug )
-  .pipe( pug() )
-  .pipe( gulp.dest( paths.build.html ) )
+	return gulp.src( paths.source.pug )
+	.pipe( pug() )
+	.pipe( gulp.dest( paths.build.html ) )
 } )
 
 // Have one task that completes all build tasks
-gulp.task('build', ['clean', 'styles', 'scripts', 'views'], () => {})
+gulp.task('build', ['clean', 'styles', 'fonts', 'dependency-scripts', 'scripts', 'views'], () => {})
 
 // Watch the source directory for changes and compile when changes are detected
 gulp.task( 'watch', ['build'], () => {
-  return watch( paths.source.root, () => {
-    gulp.start( 'build' )
-  } )
+	return watch( paths.source.root, () => {
+		gulp.start( 'build' )
+	} )
 } )
